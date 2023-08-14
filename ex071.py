f5fb6330280fd41bd87f69926c73114bae2f8606
Wellington Ferreira <141442241+WoodyWell@users.noreply.github.com>
2023-08-13 21:12:34 -0300

    Add files via upload

---
+++ ex071.py
@@ -0,0 +1,23 @@
+print('==='*10)
+print('BANCO WELL'.center(30))
+print('==='*10)
+valor = int(input('Informe o valor que deseja sacar: R$'))
+total = valor
+cedula = 50
+totced = 0
+while True:
+    if total >= cedula:
+        total -= cedula
+        totced += 1
+    else:
+        if totced > 0:
+            print(f'Total de {totced} cédulas de R${cedula}')
+        if cedula == 50:
+            cedula = 20
+        elif cedula == 20:
+            cedula = 10
+        elif cedula == 10:
+            cedula = 1
+        totced = 0
+        if total == 0:
+            break
